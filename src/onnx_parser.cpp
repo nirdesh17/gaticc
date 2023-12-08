3ce1722c60fb6ab83aae9d5ffdc2d7987a817094
Shreeyash Pandey <shreeyash335@gmail.com>
2023-12-08 15:13:44 +0530

    onnx_parser: add parse support for initializer and value_info
    Signed-off-by: Shreeyash Pandey <shreeyash335@gmail.com>

--- src/onnx_parser.cpp
+++ src/onnx_parser.cpp
@@ -8,8 +8,16 @@
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/graph_traits.hpp>
 
+#define CONV_WEIGHT_TENSOR_DIMS 4
+#define GEMM_WEIGHT_TENSOR_DIMS 2
+#define BIAS_TENSOR_DIMS 1
+
 const char *Op::LayerBase::op_type() const { return "(null)"; }
 const char *Op::LayerBase::params() const { return "(null)"; }
+void Op::LayerBase::set_initializer_params(onnx::TensorProto &t) {
+}
+void Op::LayerBase::set_value_info_params(onnx::ValueInfoProto &t) {
+}
 
 Op::Layer::Conv::Conv(ConvParams &cp) {
   std::memcpy(&m_cp, &cp, sizeof(ConvParams));
@@ -18,9 +26,45 @@ Op::Layer::Conv::Conv(ConvParams &cp) {
 const char *Op::Layer::Conv::op_type() const { return m_optype; }
 const char *Op::Layer::Conv::params() const {
   static char ret[64];
-  sprintf(ret, "IW,IH,KN,K,S,P: %d,%d,%d,%d,%d,%d", m_cp.imap[0], m_cp.imap[1],
-          m_cp.kn, m_cp.k[0], m_cp.stride[0], m_cp.pad[0]);
+  sprintf(ret, "(IW,IH: %d,%d), (KN,IC,KW,KH: %d,%d,%d,%d), (S,P: %d,%d)", 
+      m_cp.imap[0], m_cp.imap[1],
+      m_cp.kn, m_cp.ic, m_cp.k[0], m_cp.k[1],
+          m_cp.stride[0], m_cp.pad[0]);
   return ret;
+}
+void Op::Layer::Conv::set_initializer_params(onnx::TensorProto &t) {
+  if (t.dims_size() == CONV_WEIGHT_TENSOR_DIMS) {
+    m_cp.kn = t.dims()[0];
+    m_cp.ic = t.dims()[1];
+    m_cp.k[0] = t.dims()[2];
+    m_cp.k[1] = t.dims()[3];
+    weights = &t;   
+  }
+  else if (t.dims_size() == BIAS_TENSOR_DIMS) {
+    bias = &t;   
+  }
+}
+
+void Op::Layer::Conv::set_value_info_params(onnx::ValueInfoProto &t) {
+  if (t.has_type()) {
+    onnx::TypeProto type = t.type();
+    if (type.has_tensor_type()) {
+      onnx::TypeProto_Tensor tensor = type.tensor_type();
+      if (tensor.has_shape()) {
+        onnx::TensorShapeProto shape = tensor.shape();
+        assert(shape.dim_size() == CONV_WEIGHT_TENSOR_DIMS &&
+               "Value info expected conv dimensions to be 4");
+        if (shape.dim_size() == CONV_WEIGHT_TENSOR_DIMS) {
+          auto dims = shape.dim();
+          std::for_each(dims.begin(), dims.end(),
+                        [](auto &val) { assert(val.has_dim_value()); });
+          m_cp.ic = dims.at(1).dim_value();
+          m_cp.imap[0] = dims.at(2).dim_value();
+          m_cp.imap[1] = dims.at(3).dim_value();
+        }
+      }
+    }
+  }
 }
 
 const char *Op::Layer::Relu::op_type() const { return m_optype; }
@@ -38,8 +82,39 @@ Op::Layer::Gemm::Gemm(GemmParams &cp) { std::memcpy(&m_cp, &cp, sizeof(cp)); }
 const char *Op::Layer::Gemm::op_type() const { return m_optype; }
 const char *Op::Layer::Gemm::params() const {
   static char ret[64];
-  sprintf(ret, "wr,wc,is: %d,%d,%d", m_cp.wr, m_cp.wc, m_cp.is);
+  sprintf(ret, "WR,WC,IS: %d,%d,%d", m_cp.wr, m_cp.wc, m_cp.is);
   return ret;
+}
+
+void Op::Layer::Gemm::set_initializer_params(onnx::TensorProto &t) {
+  if (t.dims_size() == GEMM_WEIGHT_TENSOR_DIMS) {
+    m_cp.wr = t.dims()[0];
+    m_cp.wc = t.dims()[1];
+    weights = &t;   
+  }
+  else if (t.dims_size() == BIAS_TENSOR_DIMS) {
+    bias = &t;   
+  }
+}
+
+void Op::Layer::Gemm::set_value_info_params(onnx::ValueInfoProto &t) {
+  if (t.has_type()) {
+    onnx::TypeProto type = t.type();
+    if (type.has_tensor_type()) {
+      onnx::TypeProto_Tensor tensor = type.tensor_type();
+      if (tensor.has_shape()) {
+        onnx::TensorShapeProto shape = tensor.shape();
+        assert(shape.dim_size() == GEMM_WEIGHT_TENSOR_DIMS &&
+               "Value info expected conv dimensions to be 4");
+        if (shape.dim_size() == GEMM_WEIGHT_TENSOR_DIMS) {
+          auto dims = shape.dim();
+          std::for_each(dims.begin(), dims.end(),
+                        [](auto &val) { assert(val.has_dim_value()); });
+          m_cp.is = dims.at(1).dim_value();
+        }
+      }
+    }
+  }
 }
 
 void Op::Model::add(Op::LayerBase *layer, onnx::NodeProto &node) {
@@ -58,11 +133,32 @@ void Op::Model::add(Op::LayerBase *layer, onnx::NodeProto &node) {
     if (itr != output_map.end()) {
       boost::add_edge((*itr).second, v, g);
     }
+    auto itr2 = value_info_map.find(inputs.at(i));
+    if (itr2 != value_info_map.end()) {
+      layer->set_value_info_params(itr2->second);
+    }
+  }
+  for (int i = 0; i < inputs.size(); ++i) {
+    auto itr = input_map.find(inputs.at(i));
+    if (itr != input_map.end()) {
+      layer->set_initializer_params(itr->second);
+    }
   }
 }
 
 void Op::Model::save_initializers(onnx::TensorProto &t) {
   input_map.insert({t.name(), t});
+}
+
+void Op::Model::save_value_info(onnx::ValueInfoProto &t) {
+  value_info_map.insert({t.name(), t});
+}
+
+void Op::Model::save_first_layer_input_dims(onnx::ValueInfoProto &t) {
+  auto vertices = boost::vertices(g);
+  auto first_node = vertices.first;
+  LayerBase *layer = g[*first_node];
+  layer->set_value_info_params(t);
 }
 
 size_t Op::Model::size(void) { return boost::num_vertices(g); }
@@ -112,6 +208,18 @@ Op::Parser::Parser(std::string filename) {
   p.ParseFromIstream(&in);
   onnx::GraphProto graph = p.graph();
 
+  /* value info */
+  auto value_infos = graph.value_info();
+  for (int i = 0; i < value_infos.size(); ++i) {
+    m_model.save_value_info(value_infos.at(i));
+  }
+
+  /* initializers */
+  auto initializers = graph.initializer();
+  for (int i = 0; i < initializers.size(); ++i) {
+    m_model.save_initializers(initializers.at(i));
+  }
+
   /* nodes */
   auto nodes = graph.node();
   for (int i = 0; i < nodes.size(); ++i) {
@@ -127,11 +235,13 @@ Op::Parser::Parser(std::string filename) {
       m_model.add(new Op::Layer::Gemm(params), nodes.at(i));
     }
   }
-
-  auto initializers = graph.initializer();
-  for (int i = 0; i < initializers.size(); ++i) {
-    m_model.save_initializers(initializers.at(i));
-  }
+  
+  /* input dimensions to the first layer are stored in graph.input
+   * and needs special treatment
+   */
+  auto graph_inputs = graph.input();
+  assert(graph_inputs.size() == 1 && "Expect graph to only have 1 input");
+  m_model.save_first_layer_input_dims(graph_inputs.at(0));
 }
 
 void visitor(Op::Vertex &v, Op::Graph &g) {
--- src/onnx_parser.h
+++ src/onnx_parser.h
@@ -13,11 +13,12 @@
 namespace Op {
 
 struct ConvParams {
-  int imap[2];
-  int kn;
-  int k[2];
-  int pad[4];
-  int stride[2];
+  int imap[2]; /* input feature map */
+  int kn; /* total number of kernels */
+  int ic; /* input channels */
+  int k[2]; /* kernel width/height */
+  int pad[4]; /* padding across all four sides */
+  int stride[2]; /* stride horizontally/vertically */
 };
 
 struct ClipParams {
@@ -34,6 +35,8 @@ struct LayerBase {
   std::string name;
   virtual const char *op_type() const;
   virtual const char *params() const;
+  virtual void set_initializer_params(onnx::TensorProto &t);
+  virtual void set_value_info_params(onnx::ValueInfoProto &t);
 };
 
 void extract_conv_attr(onnx::NodeProto &node, ConvParams &params);
@@ -43,11 +46,15 @@ void extract_gemm_attr(onnx::NodeProto &node, ConvParams &params);
 namespace Layer {
 
 struct Conv : public LayerBase {
+  onnx::TensorProto *weights;
+  onnx::TensorProto *bias;
   const char *m_optype = "Conv";
   ConvParams m_cp;
   Conv(ConvParams &cp);
   const char *op_type() const override;
   const char *params() const override;
+  void set_initializer_params(onnx::TensorProto &t) override;
+  void set_value_info_params(onnx::ValueInfoProto &t) override;
 };
 
 struct Relu : public LayerBase {
@@ -65,12 +72,17 @@ struct Clip : public LayerBase {
 };
 
 struct Gemm : public LayerBase {
+  onnx::TensorProto *weights;
+  onnx::TensorProto *bias;
   const char *m_optype = "Gemm";
   GemmParams m_cp;
   Gemm(GemmParams &cp);
   const char *op_type() const override;
   const char *params() const override;
+  void set_initializer_params(onnx::TensorProto &t) override;
+  void set_value_info_params(onnx::ValueInfoProto &t) override;
 };
+
 
 } // namespace Layer
 
@@ -86,10 +98,13 @@ class Model {
   /* maps an output from a node its corresponding vertex in 'g' */
   std::map<std::string, Op::Vertex &> output_map;
   std::map<std::string, onnx::TensorProto &> input_map;
+  std::map<std::string, onnx::ValueInfoProto &> value_info_map;
 
 public:
   void add(LayerBase *layer, onnx::NodeProto &node);
   void save_initializers(onnx::TensorProto &t);
+  void save_value_info(onnx::ValueInfoProto &t);
+  void save_first_layer_input_dims(onnx::ValueInfoProto &t);
   void summary(void) const;
   size_t size(void);
   size_t size(void) const;
