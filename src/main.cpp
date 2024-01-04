58c9bda2374d7a92750bb5c4be2b583659e4b59f
Shreeyash Pandey <shreeyash335@gmail.com>
2024-01-04 18:41:12 +0530

    main: add time_estimate
    Signed-off-by: Shreeyash Pandey <shreeyash335@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,7 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
+#if 0 
   PyEngine engine("src.preprocess");
   Imgdims id{3, 224, 224};
   Kerneldims kd{1, 3, 3};
@@ -27,13 +28,12 @@ int main(int argc, char *argv[]) {
   std::cout << "out size: " << out.size() << '\n';
 
   Op::Model m;
-#if 0 
   m.add(new Op::Layer::Conv(224, 224, 64, 3));
   m.add(new Op::Layer::Relu(10));
   std::cout << m[0]->op_type() << '\n';
   std::cout << m[1]->op_type() << '\n';
 #endif
   Op::Parser parser(argv[1]);
-  parser.summary();
-  
+  //parser.summary();
+  parser.time_estimate(9, 16, 8);
 }
