c03ff49baa2155540f7c64831aab61e0c6558a69
Shreeyash Pandey <shreeyash335@gmail.com>
2024-01-30 12:05:43 +0530

    main: add Argparse
    Signed-off-by: Shreeyash Pandey <shreeyash335@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,64 @@
+#include <iostream>
 #include <numeric>
 
+#include "argagg.h"
 #include "ffi.h"
+#include "onnx_parser.h"
 #include "ops.h"
 #include "sim.h"
 #include "transformers.h"
 #include "utils.h"
-#include "onnx_parser.h"
 
-int main(int argc, char *argv[]) {
-  if (argc < 2) {
-    log_fatal("Too few arguments");
+class Argparse {
+  const char *usage = "Usage: sysim [OPTIONS]\n";
+  argagg::parser_results args;
+  argagg::parser argparser{
+      {{"help", {"-h", "--help"}, "get this help message nigga", 0},
+       {"verbose", {"-v", "--v"}, "verbose", 0},
+       {"onnx", {"--onnx"}, "load onnx file", 1}}};
+
+public:
+  Argparse(int argc, char *argv[]) {
+    if (argc < 2) {
+      std::cerr << usage << argparser;
+      log_fatal("Too few arguments");
+    }
+    try {
+      args = argparser.parse(argc, argv);
+    } catch (const std::exception &e) {
+      std::cerr << usage << argparser;
+      log_fatal("%s", e.what());
+    }
   }
 
-#if 0 
+  argagg::option_results &operator[](const std::string &name) {
+    return args[name];
+  }
+
+  bool has_option(const std::string &name) const {
+    return args.has_option(name);
+  }
+
+  void print_usage() const {
+    std::cerr << usage << argparser;
+  }
+};
+
+int main(int argc, char *argv[]) {
+  Argparse args(argc, argv);
+
+  if (args.has_option("help")) {
+    args.print_usage();
+  }
+
   PyEngine engine("src.preprocess");
-  Imgdims id{3, 224, 224};
-  Kerneldims kd{1, 3, 3};
-  int sa_rows = 9;
-  int sa_cols = 1;
-
   std::vector<int> img = py_read_img(engine, std::string("images/mug.jpg"));
-  auto weights = py_fetch_kernel(engine, 0, 0, 1);
-  SA a1(sa_rows, sa_cols);
-  Conv2d cc(id, kd);
-  auto out = cc(img, weights, a1);
-  std::cout << "out size: " << out.size() << '\n';
+  //print_vec<int>("img", img);
 
-  Op::Model m;
-  m.add(new Op::Layer::Conv(224, 224, 64, 3));
-  m.add(new Op::Layer::Relu(10));
-  std::cout << m[0]->op_type() << '\n';
-  std::cout << m[1]->op_type() << '\n';
-#endif
-  Op::Parser parser(argv[1]);
-  parser.get_execution_order();
-  //parser.time_estimate(9, 8, 8);
+  if (args.has_option("onnx")) {
+    std::string s = args["onnx"].as<std::string>();
+    Op::Parser parser(s);
+    parser.get_execution_order();
+    parser.time_estimate(9, 8, 8);
+  }
 }
