1d6c759af36b4ace51bc506222c548021d2a7166
Mir-Aatif-Rafiq <bitemiraatif@gmail.com>
2024-01-31 16:42:24 +0530

    refactor(sasa_test.cpp) : new working test w.r.t. tensors

--- tests/sasa_test.cpp
+++ tests/sasa_test.cpp
@@ -15,8 +15,8 @@ int main() {
   CP1.kn = 64;
   CP1.stride[0]=1;
   CP1.stride[1]=1;
-  CP1.imap[0]= 224;
-  CP1.imap[1]= 224;
+  CP1.imap[0]= 300;
+  CP1.imap[1]= 300;
   Op::Layer::Conv conv1(CP1);
   
   
@@ -26,25 +26,36 @@ int main() {
   onnx::ModelProto MP1;
   onnx::GraphProto* GP1;
   onnx::AttributeProto* AP1;
-  std::fstream input("<onnx/file/location>",std::ios::in);
+  std::fstream input("/home/mir_aatif_rafiq/Downloads/vgg16-12-int8.onnx",std::ios::in);
     MP1.ParseFromIstream(&input);
     GP1 = MP1.mutable_graph();
     conv1.weights= GP1->mutable_initializer(2);
-    TensorExtant<int8_t> TE1(conv1.weights);
-  Tensor<int8_t>& tensor1 = TE1;
   std::vector<int> temp_dims{0,0,0,0};
-  std::vector<int> create_dim{3,224,224};
-  TensorCreate<int8_t> TC1(create_dim);
-  for(int i = 0 ; i < TC1.dims_iterator(-1); i ++){
+  TensorCreate<int8_t> TC1;
+  for(int i = 0 ; i < 3*300*300; i ++){
     TC1.push_back(i%127);
   }
+  std::vector<int> create_dim{3,300,300};
+  TC1.set_dims(create_dim,0);
   Tensor<int8_t>& tensor2 = TC1;
   SASA s1(9, 8, 8, conv1);
-  output = s1.master<int8_t>(tensor2);
+  std::vector<int> output_dims{conv1.m_cp.kn,(sa_output_dims(conv1.m_cp.imap[0], conv1.m_cp.pad[0] /*padding*/,
+                       1 /*dilation*/, conv1.m_cp.k[0],
+                       conv1.m_cp.stride[0] /*stride*/) ),
+        sa_output_dims(conv1.m_cp.imap[1], conv1.m_cp.pad[0] /*padding*/,
+                       1 /*dilation*/, conv1.m_cp.k[1],
+                       conv1.m_cp.stride[1] /*stride*/)};
+
+
+  TensorCreate<int8_t> tensor_output(output_dims);
+  Tensor<int8_t>& output_tensor = tensor_output;
+  s1.master<int8_t>(tensor2,output_tensor);
+  printf("output tensor size %d \n",output_tensor.size());
+  // std::cout<<"output tensor size "<<output_tensor.size()<<std::endl;
   // print_vec_vec(" output is ",output.at(0));
-  std::cout << "output kernel size : " << output.size() << std::endl;
-  std::cout << "output row size : " << output.at(0).size() << std::endl;
-  std::cout << "output col size : " << output.at(0).at(0).size() << std::endl;
+  // std::cout << "output kernel size : " << output.size() << std::endl;
+  // std::cout << "output row size : " << output.at(0).size() << std::endl;
+  // std::cout << "output col size : " << output.at(0).at(0).size() << std::endl;
 
   auto stop = std::chrono::high_resolution_clock::now();
   auto duration =
@@ -55,3 +66,8 @@ int main() {
 
   return 0;
 }
+
+
+
+// must in documents
+// if decided to use tensor.pushback , make sure to set it dims afterwards and make sure to ONLY use pushback when used default constructor of TensorCreate
