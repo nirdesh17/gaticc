2f16f300af7b7326adc757220a1f2efff18a7497
nirdesh17 <nirdeshdevadiya17@gmail.com>
2024-10-11 14:53:06 +0530

    chore(conv_engine test): restructure and clean test files

--- tests/conv_engine_1x1x3x3_1x1x2x2.cpp
+++ tests/conv_engine_1x1x3x3_1x1x2x2.cpp
@@ -9,94 +9,66 @@
 #include "../src/utils.h"
 #include "Python.h"
 
-
-// input (1,3,3,3) and 1 kernals(1,3,2,2)
-void three_by_two3() {
+bool conv_engine_1x1x3x3_1x1x2x2() {
     
-    std::vector<int> input_dims = {1, 3, 3, 3}; 
-    std::vector<float> input_values(input_dims[0]*input_dims[1]*input_dims[2] * input_dims[3]);
+    std::vector<int> input_dims = {1, 1, 3, 3}; 
+    std::vector<float> input_values(input_dims[2] * input_dims[3]);
     std::iota(input_values.begin(), input_values.end(), 0);
-
     TensorCreate<float> input(input_values, input_dims);
-    
-    std::vector<int> weight_dims = {1, 3, 2, 2}; 
+
+    std::vector<int> weight_dims = {1, 1, 2, 2}; 
     onnx::TensorProto weight_proto;
     weight_proto.set_name("weight");
-    for(int i = 0; i < weight_dims.size(); i++) {
-        weight_proto.add_dims(weight_dims[i]);
-    }
-    std::vector<float> weight_values(weight_dims[0] * weight_dims[1]*weight_dims[2] * weight_dims[3]);
+    weight_proto.mutable_dims()->Add(weight_dims.begin(), weight_dims.end());
+    std::vector<float> weight_values(weight_dims[2] * weight_dims[3]);
     std::iota(weight_values.begin(), weight_values.end(), 0);
-    for(int i = 0; i < weight_dims[0] * weight_dims[1]*weight_dims[2] * weight_dims[3]; i++) {
-        weight_proto.add_float_data(weight_values[i]);
-    }
+    weight_proto.mutable_float_data()->Add(weight_values.begin(), weight_values.end());
     weight_proto.set_data_type(onnx::TensorProto::FLOAT);
 
     onnx::TensorProto bias_proto;
     bias_proto.set_name("bias");
-    bias_proto.add_dims(3);  
-    bias_proto.add_float_data(0);
-    bias_proto.add_float_data(0);
+    bias_proto.add_dims(1);   
     bias_proto.add_float_data(0);
     bias_proto.set_data_type(onnx::TensorProto::FLOAT);
  
-    
     std::vector<int> output_dims = {1, 1, 2, 2}; 
     std::vector<float> expected_output_values = {
-        1035, 1102, 1257, 1299
+        19,25,37,43
     };
     TensorCreate<float> expected_output(expected_output_values, output_dims);
     
-
     TensorCreate<float> output(output_dims);
-
-
-    Op::ConvParams conv_params;
-    conv_params.kn = 3;
-    conv_params.k[0] = 2;
-    conv_params.k[1] = 2;
-    conv_params.stride[0] = 1;
-    conv_params.stride[1] = 1;
-    conv_params.pad[0] = 0;
-    conv_params.pad[1] = 0;
-    conv_params.pad[2] = 0;
-    conv_params.pad[3] = 0;
-
-   
-
+    
+    Op::ConvParams conv_params={1,{2, 2},{0,0,0,0},{1,1},{0, 0}};
+      
     Op::Layer::Conv conv_layer;
     conv_layer.m_cp = conv_params;
     conv_layer.weights = &weight_proto;
     conv_layer.bias = &bias_proto;
     conv_layer.output_dims= output_dims;
-
+    conv_layer.input_dims = input_dims;
     
-
     ConvEngine<float, float, float> conv_engine(&conv_layer);
 
-
-    
     conv_engine.run(&input, &output);
 
+    std::vector<float> output_values = output.get();
 
-    for (int i = 0; i < output.size(); ++i) {
-        assert(output.at(i) == expected_output.at(i));
-    }
-
-    std::cout << "Test passed!" << std::endl;
+    bool result = generate_report<float, float>("conv_engine_1x1x3x3_1x1x2x2", output_values, expected_output_values);
+    return result;
 }
-
-
 
 Argparse gbl_args;
 void global_init(int argc, char *argv[]) {
   gbl_args.parse(argc, argv);
   Py_Initialize();
 }
+
 int main(int argc, char *argv[]) {
   global_init(argc, argv);
   import_array();
-  three_by_two3();
+  if(!conv_engine_1x1x3x3_1x1x2x2())
+    exit(1);
 
     return 0;
 }
--- tests/conv_engine_1x1x5x3_1x1x3x3.cpp
+++ tests/conv_engine_1x1x5x3_1x1x3x3.cpp
@@ -9,82 +9,54 @@
 #include "../src/utils.h"
 #include "Python.h"
 
-void five_by_three() {
+bool conv_engine_1x1x5x3_1x1x3x3() {
     
     std::vector<int> input_dims = {1, 1, 5, 3}; 
     std::vector<float> input_values(input_dims[2] * input_dims[3]);
     std::iota(input_values.begin(), input_values.end(), 0);
-
     TensorCreate<float> input(input_values, input_dims);
-
 
     std::vector<int> weight_dims = {1, 1, 3, 3}; 
     onnx::TensorProto weight_proto;
     weight_proto.set_name("weight");
-    for(int i = 0; i < weight_dims.size(); i++) {
-        weight_proto.add_dims(weight_dims[i]);
-    }
+    weight_proto.mutable_dims()->Add(weight_dims.begin(), weight_dims.end());
     std::vector<float> weight_values(weight_dims[2] * weight_dims[3]);
     std::iota(weight_values.begin(), weight_values.end(), 0);
-    for(int i = 0; i < weight_dims[2] * weight_dims[3]; i++) {
-        weight_proto.add_float_data(weight_values[i]);
-    }
-
+    weight_proto.mutable_float_data()->Add(weight_values.begin(), weight_values.end());
     weight_proto.set_data_type(onnx::TensorProto::FLOAT);
 
-    
     onnx::TensorProto bias_proto;
     bias_proto.set_name("bias");
     bias_proto.add_dims(1);   
     bias_proto.add_float_data(0);
     bias_proto.set_data_type(onnx::TensorProto::FLOAT);
  
-    
-
     std::vector<int> output_dims = {1, 1, 3, 1}; 
     std::vector<float> expected_output_values = {
         204, 312, 420
     };
     TensorCreate<float> expected_output(expected_output_values, output_dims);
     
-
     TensorCreate<float> output(output_dims);
     
-
-    Op::ConvParams conv_params;
-    conv_params.kn = 1;
-    conv_params.k[0] = 3;
-    conv_params.k[1] = 3;
-    conv_params.stride[0] = 1;
-    conv_params.stride[1] = 1;
-    conv_params.pad[0] = 0;
-    conv_params.pad[1] = 0;
-    conv_params.pad[2] = 0;
-    conv_params.pad[3] = 0;
-
-   
-
+    Op::ConvParams conv_params={1,{3, 3},{0,0,0,0},{1,1},{0, 0}};
+     
     Op::Layer::Conv conv_layer;
     conv_layer.m_cp = conv_params;
     conv_layer.weights = &weight_proto;
     conv_layer.bias = &bias_proto;
     conv_layer.output_dims= output_dims;
-
-    
+    conv_layer.input_dims = input_dims;
 
     ConvEngine<float, float, float> conv_engine(&conv_layer);
 
-    
     conv_engine.run(&input, &output);
-
   
-    for (int i = 0; i < output.size(); ++i) {
-        assert(output.at(i) == expected_output.at(i));
-    }
+    std::vector<float> output_values = output.get();
 
-    std::cout << "Test passed!" << std::endl;
+    bool result = generate_report<float, float>("conv_engine_1x1x5x3_1x1x3x3", output_values, expected_output_values);
+    return result;
 }
-
 
 Argparse gbl_args;
 void global_init(int argc, char *argv[]) {
@@ -95,7 +67,8 @@ void global_init(int argc, char *argv[]) {
 int main(int argc, char *argv[]) {
   global_init(argc, argv);
   import_array();
-  five_by_three();
+  if(!conv_engine_1x1x5x3_1x1x3x3())
+    exit(1);
 
     return 0;
 }
