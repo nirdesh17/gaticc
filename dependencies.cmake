26119f635163b205b78f80a9fa3e44ca7a97b60e
Shreeyash Pandey <shreeyash335@gmail.com>
2024-10-24 16:35:59 +0530

    fix(CMakeLists): include abseil files for protbufs version > 23
    Signed-off-by: Shreeyash Pandey <shreeyash335@gmail.com>

--- dependencies.cmake
+++ dependencies.cmake
@@ -1,10 +1,6 @@
 find_package(Python3 COMPONENTS Interpreter Development REQUIRED)
 find_package(Python3 COMPONENTS NumPy REQUIRED)
 find_package(Boost 1.67 COMPONENTS REQUIRED)
-# Abseil is required by Protobuf and not sysim directly. Ideally, FindProtobuf.cmake
-# should include and link abseil libs to sysim but on Arch Linux this seems to be 
-# broken. This is a quick fix.
-find_package(absl REQUIRED)
 find_package(Protobuf REQUIRED)
 
 set(ALL_DIR
@@ -19,7 +15,20 @@ set(ALL_LIBS
     ${Boost_LIBRARIES}
     Python3::Python
     protobuf::libprotobuf
-    #absl_log_internal_message
-    #absl_log_internal_check_op
     dl
 )
+
+set(PROTOBUF_ERR_VERSION "0.23")
+# protobuf has a peculiar bug where on versions greater than PROTOBUF_ERR_VERSION
+# cannot link to absl_log_* libs. 
+# See: https://github.com/protocolbuffers/protobuf/issues/11920
+# Although the issue is closed (triaged by googlebot), the issue
+# still persists as of latest version (libprotobuf 28)
+# See: https://duckduckgo.com/?q=protbuf+absel+linker+errors
+if (${Protobuf_VERSION} VERSION_GREATER ${PROTOBUF_ERR_VERSION})
+  # Abseil is required by Protobuf and not sysim directly. Ideally, FindProtobuf.cmake
+  # should include and link abseil libs to sysim but on Arch Linux this seems to be 
+  # broken. This is a quick fix.
+  find_package(absl REQUIRED)
+  list(APPEND ALL_LIBS absl_log_internal_message absl_log_internal_check_op)
+endif()
