f51557878050ecb8cc648a284d6b5e168d175f49
Shreeyash Pandey <shreeyash335@gmail.com>
2024-11-11 14:42:34 +0530

    feat(scripts): add install_deps_vaaman.sh
    Signed-off-by: Shreeyash Pandey <shreeyash335@gmail.com>

---
+++ scripts/install_deps_vaaman.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt install \
+  libprotobuf23 \
+  libprotobuf-dev \
+  protobuf-compiler \
+  libboost-graph-dev \
+  python3-dev \
+  python3 \
+  pkg-config \
+  python3-numpy
