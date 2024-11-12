3e47cd08fe513c4407db86b4592b2027ee25e223
Shreeyash Pandey <shreeyash335@gmail.com>
2024-11-12 14:08:44 +0530

    fix(sim): move inlined functions to .cpp, uninline them
    Signed-off-by: Shreeyash Pandey <shreeyash335@gmail.com>

--- src/sim.cpp
+++ src/sim.cpp
@@ -31,3 +31,69 @@ std::vector<float> compute_output_scale(const std::vector<float>& x_scale,
   }
   return ret;
 }
+
+std::vector<int> permute(const std::vector<int> &v,
+                                  std::vector<int> perm) {
+  std::for_each(perm.begin(), perm.end(),
+                [&v](int i) { assert((i < v.size()) ? true : false); });
+  std::vector<int> ret(v.size());
+  for (int i = 0; i < v.size(); ++i) {
+    ret.at(i) = v.at(perm.at(i));
+  }
+  return ret;
+}
+
+/* n-dimensional-index incrementer
+ * ii is a n-dimensional index
+ * limit_shape is the shape of the tensor that ii is indexing
+ *
+ * for example,
+ * consider, limit_shape = [3,4,2]
+ * first,
+ *  ii = [0,0,0]
+ * calling increment_shape on it makes it,
+ *  ii = [0,0,1]
+ * then,
+ *  ii = [0,1,0]
+ *  ii = [0,1,1]
+ *  ii = [0,2,0]
+ *  ii = [0,2,1]
+ *  ii = [0,3,0]
+ *  ii = [0,3,1]
+ *  ii = [1,0,0]
+ * and so on till
+ *  ii = [2,3,1]
+ */
+void increment_shape(std::vector<int> &ii, const std::vector<int> &limit_shape) {
+  assert(ii.size() == limit_shape.size());
+  int current_index = ii.size() - 1;
+  while (current_index >= 0) {
+    ii.at(current_index)++;
+    if (ii.at(current_index) >= limit_shape.at(current_index)) {
+      ii.at(current_index) = 0;
+      current_index--;
+    } else {
+      break;
+    }
+  }
+  if (ii.at(0) >= limit_shape.at(0)) {
+    log_fatal("Cannot increment past limit_shape");
+  }
+}
+
+/* Deduces and removes -1/0 from old_shape to return 
+ * a correct new_shape.
+ * See https://onnx.ai/onnx/operators/onnx__Reshape.html#reshape
+ *
+ * TODO: handle 0s in shape (does not do it presently)
+ */
+std::vector<int64_t> deduce_new_shape(std::vector<int64_t> old_shape, int input_total_size) {
+  auto itr = std::find(old_shape.begin(), old_shape.end(), -1);
+  if (itr != old_shape.end()) {
+    int remaining_size = std::abs(prod(old_shape.begin(), old_shape.end(), 1));
+    assert(input_total_size % remaining_size == 0 && "unable to deduce new shape");
+    int remaining_dim = input_total_size / remaining_size;
+    *itr = remaining_dim; 
+  }
+  return old_shape;
+}
--- src/sim.h
+++ src/sim.h
@@ -88,16 +88,7 @@ void flatten(const Tensor<T> *input, Tensor<T> *output) {
   output->set_dims(new_dims);
 }
 
-inline std::vector<int> permute(const std::vector<int> &v,
-                                  std::vector<int> perm) {
-  std::for_each(perm.begin(), perm.end(),
-                [&v](int i) { assert((i < v.size()) ? true : false); });
-  std::vector<int> ret(v.size());
-  for (int i = 0; i < v.size(); ++i) {
-    ret.at(i) = v.at(perm.at(i));
-  }
-  return ret;
-}
+std::vector<int> permute(const std::vector<int> &v, std::vector<int> perm);
 
 template <typename T>
 std::valarray<T> vec2val(const std::vector<T> &v) {
@@ -113,43 +104,7 @@ std::valarray<T> vec2val(std::vector<T> &&v) {
   return vec2val(v);
 }
 
-/* n-dimensional-index incrementer
- * ii is a n-dimensional index
- * limit_shape is the shape of the tensor that ii is indexing
- *
- * for example,
- * consider, limit_shape = [3,4,2]
- * first,
- *  ii = [0,0,0]
- * calling increment_shape on it makes it,
- *  ii = [0,0,1]
- * then,
- *  ii = [0,1,0]
- *  ii = [0,1,1]
- *  ii = [0,2,0]
- *  ii = [0,2,1]
- *  ii = [0,3,0]
- *  ii = [0,3,1]
- *  ii = [1,0,0]
- * and so on till
- *  ii = [2,3,1]
- */
-inline void increment_shape(std::vector<int> &ii, const std::vector<int> &limit_shape) {
-  assert(ii.size() == limit_shape.size());
-  int current_index = ii.size() - 1;
-  while (current_index >= 0) {
-    ii.at(current_index)++;
-    if (ii.at(current_index) >= limit_shape.at(current_index)) {
-      ii.at(current_index) = 0;
-      current_index--;
-    } else {
-      break;
-    }
-  }
-  if (ii.at(0) >= limit_shape.at(0)) {
-    log_fatal("Cannot increment past limit_shape");
-  }
-}
+void increment_shape(std::vector<int> &ii, const std::vector<int> &limit_shape);
 
 /* TODO: use valarray where fits */
 template <typename T>
@@ -297,22 +252,7 @@ void VA<inputT, weightT, biasT, outputT>::run(const Tensor<inputT> *input, Tenso
   }
 }
 
-/* Deduces and removes -1/0 from old_shape to return 
- * a correct new_shape.
- * See https://onnx.ai/onnx/operators/onnx__Reshape.html#reshape
- *
- * TODO: handle 0s in shape (does not do it presently)
- */
-inline std::vector<int64_t> deduce_new_shape(std::vector<int64_t> old_shape, int input_total_size) {
-  auto itr = std::find(old_shape.begin(), old_shape.end(), -1);
-  if (itr != old_shape.end()) {
-    int remaining_size = std::abs(prod(old_shape.begin(), old_shape.end(), 1));
-    assert(input_total_size % remaining_size == 0 && "unable to deduce new shape");
-    int remaining_dim = input_total_size / remaining_size;
-    *itr = remaining_dim; 
-  }
-  return old_shape;
-}
+std::vector<int64_t> deduce_new_shape(std::vector<int64_t> old_shape, int input_total_size);
 
 template <typename T>
 void reshape(const Tensor<T> *input, Tensor<T> *output, const std::vector<int64_t> &new_shape) {
