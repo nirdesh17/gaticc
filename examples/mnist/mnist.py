3571d50101b89c66d56f49262ad3df7d06c40446
Shreeyash Pandey <shreeyash335@gmail.com>
2025-02-26 16:52:19 +0530

    feat(python): add gati.py as a python frontend for gati

--- examples/mnist/mnist.py
+++ examples/mnist/mnist.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import numpy as np
+import os
+import gati
 
 def gen():
     arr = np.load("mnist_10.npy")
@@ -10,3 +12,15 @@ def post(num):
     m = np.argmax(num)
     print(f"number: {m}")
     return m
+
+if __name__ == "__main__":
+		onnx_path = "/home/vicharak/gaticc/models/mnistpad1_6_28_int8.onnx"
+		bitstream = "/home/vicharak/gaticc/rah_mnist_test_25_02.hex"
+		gml_name = "model.gml"
+		
+		gati.version()
+		gati.set_arch(ramsize=512, sa_arch="9,4,4", vasize=32, 
+									accbuf_size=4096, fcbuf_size=32768)
+		gati.compile(onnx_path, gml_name)
+		gati.flash(bitstream)
+		gati.run(onnx_path, gml_name, "mnist.py", "gen", "post")
