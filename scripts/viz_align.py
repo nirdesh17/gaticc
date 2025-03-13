1af68e331433cff36488e627734b41d2c6e26cd7
Shreeyash Pandey <shreeyash335@gmail.com>
2025-03-13 17:52:47 +0530

    fix(viz_align.py): BUG: do not resize image, pad zeros after
    Signed-off-by: Shreeyash Pandey <shreeyash335@gmail.com>

--- scripts/viz_align.py
+++ scripts/viz_align.py
@@ -9,7 +9,7 @@ def ceil_mod(a, b):
     return int(math.ceil(a / b) * b)
 
 def align_dims(input_dims, sa_arch, dk):
-    new_dims = [ceil_mod(input_dims[0], sa_arch[2]), ceil_mod(input_dims[1], dk), ceil_mod(input_dims[2], dk)]
+    new_dims = [ceil_mod(input_dims[0], sa_arch[2]), input_dims[1], input_dims[2]]
     return new_dims
 
 def viz_sa_input(input_dims, sa_arch, dram_width):
@@ -18,22 +18,18 @@ def viz_sa_input(input_dims, sa_arch, dram_width):
     print(f"input_dims: {input_dims}, sa_arch: {sa_arch}, dram_width: {dram_width} new_dims: {new_dims}")
     table_data = []
     for c in range(new_dims[0] // sa_arch[2]):
-        for hi in range(new_dims[1]):
-            for wi in range(new_dims[2] // dk):
-                row = []
-                for ci in range(sa_arch[2]):
-                    for ei in range(dk):
-                        chan = c * sa_arch[2] + ci
-                        h = hi
-                        w = wi * dk + ei
-                        if chan >= input_dims[0] or h >= input_dims[1] or w >= input_dims[2]:
-                            row.append("0")
-                        else:
-                            row.append(f"c{chan}h{h}w{w}")
-                table_data.append(row)
+        for e in range(ceil_mod(new_dims[1] * new_dims[2], dk) // dk):
+            row = []
+            for ci in range(sa_arch[2]):
+                for ei in range(dk):
+                    chan = c * sa_arch[2] + ci
+                    elem = e * dk + ei
+                    if chan >= input_dims[0] or elem >= input_dims[1] * input_dims[2]:
+                        row.append("0")
+                    else:
+                        row.append(f"c{chan}e{elem}")
+            table_data.append(row)
     return table_data
-    
-
 
 def main():
     parser = argparse.ArgumentParser(description="Visualize aligned dimensions for a given input")
