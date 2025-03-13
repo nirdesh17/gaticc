dbc2afa6a860c8be86f94fa6175133ca914aeed0
Shreeyash Pandey <shreeyash335@gmail.com>
2025-03-13 16:12:36 +0530

    feat(scripts): add viz_align.py
    Signed-off-by: Shreeyash Pandey <shreeyash335@gmail.com>

---
+++ scripts/viz_align.py
@@ -0,0 +1,47 @@
+import math
+import argparse
+
+input_dims = [5, 3, 5]
+sa_arch = [9, 8, 8]
+dram_width = 32
+
+def ceil_mod(a, b):
+    return int(math.ceil(a / b) * b)
+
+def align_dims(input_dims, sa_arch, dk):
+    new_dims = [ceil_mod(input_dims[0], sa_arch[2]), ceil_mod(input_dims[1], dk), ceil_mod(input_dims[2], dk)]
+    return new_dims
+
+def viz_sa_input(input_dims, sa_arch, dram_width):
+    dk = dram_width // sa_arch[2]
+    new_dims = align_dims(input_dims, sa_arch, dk)
+    print(f"input_dims: {input_dims}, sa_arch: {sa_arch}, dram_width: {dram_width} new_dims: {new_dims}")
+    table_data = []
+    for c in range(new_dims[0] // sa_arch[2]):
+        for hi in range(new_dims[1]):
+            for wi in range(new_dims[2] // dk):
+                row = []
+                for ci in range(sa_arch[2]):
+                    for ei in range(dk):
+                        chan = c * sa_arch[2] + ci
+                        h = hi
+                        w = wi * dk + ei
+                        if chan >= input_dims[0] or h >= input_dims[1] or w >= input_dims[2]:
+                            row.append("0")
+                        else:
+                            row.append(f"c{chan}h{h}w{w}")
+                table_data.append(row)
+    return table_data
+    
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Visualize aligned dimensions for a given input")
+    parser.add_argument('--input_dims', type=int, nargs=3, help="Input dimensions [depth, height, width]")
+    parser.add_argument('--sa_arch', type=int, nargs=3, help="Systolic array arch dimensions [rows, cols, N]")
+    parser.add_argument('--dram_width', type=int, help="DRAM width")
+    args = parser.parse_args()
+    viz_sa_input(args.input_dims, args.sa_arch, args.dram_width)
+
+if __name__ == "__main__":
+    main()
