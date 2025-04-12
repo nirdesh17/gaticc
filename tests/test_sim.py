7c780a276c302a8b65809832dbd4c44c4a6862a7
Shreeyash Pandey <shreeyash335@gmail.com>
2025-04-12 16:26:36 +0530

    fix(test_sim.py): use a list of files over scanning models_dir
    Signed-off-by: Shreeyash Pandey <shreeyash335@gmail.com>

--- tests/test_sim.py
+++ tests/test_sim.py
@@ -21,6 +21,34 @@ def post(num):
     with open("results.txt", "a") as f: f.write(f"{m}\n")
     return m
 
+files = [
+'cifar10_vgg11.onnx',
+'cifar10_vgg16.onnx',
+'cifar10_vgg19.onnx',
+'imagenet_vgg_16_224_int8.onnx',
+'imagenet_vgg_16_224_uint8.onnx',
+'mnist_6_28_int8.onnx',
+'mnist_average_pool_int8.onnx',
+'mnist_depthwise_60acc.onnx',
+'mnist_depthwise_63acc.onnx',
+'mnist_global_average_pool_int8.onnx',
+'mnist_int8_2x2.onnx',
+'mnist_int8_k1x11.onnx',
+'mnist_int8_k1x7.onnx',
+'mnist_int8_maxpool_k3_s3.onnx',
+'mnist_int8_pad2.onnx',
+'mnist_int8_stride2.onnx',
+'mnist_int8_stride2_pad0.onnx',
+'mnist_int8_stride2_pad2.onnx',
+'mnist_int8_stride3.onnx',
+'mnistpad1_6_28_int8.onnx',
+'mnist_qlinearadd2.onnx',
+'mnist_qlinearadd.onnx',
+'mnist_uint8.onnx',
+'mnist_uint8_pad_0.onnx',
+'mnist_uint8_tiny.onnx'
+]
+
 def get_tbl(l):
     s = ''
     for i in l:
@@ -41,7 +69,7 @@ if __name__ == "__main__":
     print(f"Using onnx models from: {models_dir}")
     gati.set_keep_quiet(True)
 
-    for file in os.listdir(models_dir):
+    for file in files:
         print(f"File: {file}")
         with open("results.txt", "w"): pass
         accuracy = 0
@@ -60,16 +88,16 @@ if __name__ == "__main__":
         else:
             ret = 1
 
-        if ret != 0:
-            failed.append(file)
+        if ret != 0 or accuracy < 50:
+            failed.append((file, accuracy))
         else:
             passed.append((file, accuracy))
 
     resfile_name = os.path.basename(sys.argv[0]).split('.')[0] + ".results.txt"
     with open(f"{resfile_name}", "w") as f:
-        f.write(f"Failed Compilations\n")
-        f.write(failed)
-        f.write(f"Passed Compilation\n")
+        f.write(f"==Failed==\n")
+        f.write(get_tbl(failed))
+        f.write(f"==Passed==\n")
         f.write(get_tbl(passed))
         f.write(f"Results: {len(passed)}/{len(passed)+len(failed)}")
     print(f"Results written to: {resfile_name}\n")
