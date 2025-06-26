d5070923aa6f6217967d7ac292e9c7a4e5427c9d
Shreeyash Pandey <shreeyash335@gmail.com>
2025-06-26 13:07:24 +0530

    fix(test_run.py): add new models
    Signed-off-by: Shreeyash Pandey <shreeyash335@gmail.com>

--- tests/test_run.py
+++ tests/test_run.py
@@ -2,10 +2,10 @@ import os, sys, json, subprocess, argparse
 
 mut = [
   'mnist_6_28_int8.onnx', 'mnist_int8_pad2.onnx', 'mnist_int8_stride2.onnx',
-  'mnist_int8_stride3.onnx', 'mnistpad1_6_28_int8.onnx', #'imagenet_vgg_16_224_int8.onnx',
+  'mnist_int8_stride3.onnx', 'mnistpad1_6_28_int8.onnx', 'imagenet_vgg_16_224_int8.onnx',
   'cifar10_vgg11.onnx', 'cifar10_vgg16.onnx', 'cifar10_vgg19.onnx',
   'mnist_int8_k1x7_nomaxpool.onnx', 'mnist_int8_stride2_pad0.onnx',
-  'mnist_int8_stride2_pad2.onnx', 'mnist_average_pool_int8.onnx'
+  'mnist_int8_stride2_pad2.onnx', 'mnist_average_pool_int8.onnx', 'mnist_int8_pad4343.onnx'
 ]
 
 def run_all(args):
