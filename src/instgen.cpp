2de4963202f930b30e4341ffba6e5826583b9c49
nirdesh17 <nirdeshdevadiya17@gmail.com>
2025-12-15 14:58:28 +0530

    test

--- src/instgen.cpp
+++ src/instgen.cpp
@@ -17,7 +17,7 @@ static std::set<std::string> megablock_tbl{
     "QLinearConv", "QGemm",      "Conv",
     "Gemm",        "QLinearEltwise", "NonMaxSuppression", "NoOp", "Transpose"};
 
-static std::set<int> megablock_opcode_tbl{OP_CONV, OP_FC, OP_EltWise, OP_NMS, OP_TRANSPOSE};
+static std::set<int> megablock_opcode_tbl{OP_CONV, OP_FC, OP_EltWise, OP_NMS, OP_TRANSPOSE, OP_RESIZE};
 
 bool is_nms(const Op::LayerBase *l) {
   return (std::string(l->op_type()) == "NonMaxSuppression");
@@ -1592,6 +1592,40 @@ int Op::Layer::Reshape::get_inst(InstBlob &blob, AddressGen &gen, InitializerTab
   return 0;
 }
 
+void Op::Layer::Resize::get_opcodes(std::vector<int> &op_codes) {
+  op_codes.push_back(OP_RESIZE);
+  op_codes.push_back(OP_OutputBlock);
+  op_codes.push_back(OP_TailBlock);
+}
+
+uint32_t Op::Layer::Resize::get_weight_size() {
+  return 0;
+}
+
+int Op::Layer::Resize::get_inst(InstBlob &blob, AddressGen &gen, InitializerTable &tbl) {
+  std::bitset<INST_SIZE_BITS> rinst;
+  inst_set(rinst, OP_RESIZE, RESIZE_Opcode);
+  auto in_dims = this->input_dims.at(0);
+  inst_set(rinst, in_dims.at(TENSOR_4D_CHANNELS), RESIZE_IC);
+  inst_set(rinst, in_dims.at(TENSOR_4D_HEIGHT), RESIZE_IH);
+  inst_set(rinst, in_dims.at(TENSOR_4D_WIDTH), RESIZE_IW);
+  uint32_t addr = gen.io_addr_from_register(this->inputs.at(0));
+  inst_set(rinst, addr, RESIZE_ImageStartAddress);
+  uint32_t addr_end = ceil_mod(addr + prod(in_dims) * Op::tpdt_sizeof(onnx::TensorProto_DataType_INT8), WORD_SIZE);
+  inst_set(rinst, addr_end, RESIZE_ImageEndAddress);
+  blob.push_back(rinst);
+
+  auto out_dims = this->output_dims.at(0);
+  uint32_t out_addr = gen.io_addr_from_register(this->outputs.at(0));
+  int ido = ceil_mod(prod(out_dims), WORD_SIZE);
+  std::bitset<INST_SIZE_BITS> oinst = gen_output(0, out_addr, 1, 1, ido, 0, 0, this->dispatch, string_hash(this->name), 0, out_dims.at(TENSOR_4D_HEIGHT), out_dims.at(TENSOR_4D_WIDTH), 1, 0, 0);
+  blob.push_back(oinst);
+  std::bitset<INST_SIZE_BITS> tinst;
+  inst_set(tinst, OP_TailBlock, TailBlock_Opcode);
+  blob.push_back(tinst);
+  return 0;
+}
+
 AddressGen::AddressGen(Op::Graph graph) : current_address{0} {
   m_exec_order = crt_exec_order(graph);
 
@@ -2412,7 +2446,7 @@ void GmlCheck::check_citr_kitr(const InstBlob &instblob) const {
       } else if (p_op == OP_NMS) {
         expected_chan_itr = 1;
         expected_kern_itr = 1;
-      } else if (p_op == OP_TRANSPOSE) {
+      } else if (p_op == OP_TRANSPOSE || p_op == OP_RESIZE) {
         expected_chan_itr = 1;
         expected_kern_itr = 1;
       } else {
@@ -2505,7 +2539,7 @@ void GmlCheck::check_weight_address_continuity(const InstBlob &instblob) const {
     } else if (op == OP_FC) {
       ret = check_fc_weight_continuity(inst);
     } else if (op == OP_OutputBlock || op == OP_START || op == OP_EltWise ||
-               op == OP_NMS || op == OP_TRANSPOSE || op == OP_RESHAPE) {
+               op == OP_NMS || op == OP_TRANSPOSE || op == OP_RESHAPE || op == OP_RESIZE) {
       // do nothing
     } else {
       log_fatal("Unhandled instruction in check_weight_address_continuity {}\n",
--- src/instructions.h
+++ src/instructions.h
@@ -416,7 +416,7 @@
 #define RESHAPE_ImageStartAddress_HIGH 67
 #define RESHAPE_ImageStartAddress_COUNT 32
 
-#define ISA_VERSION 12
+#define ISA_VERSION 13
 #define ACT_RELU 0x00
 #define ACT_CLIP 0x01
 #define ACT_LEAKYRELU 0x02
@@ -451,6 +451,30 @@
 #define CONV_TYPE_DW 1
 #define CONV_TYPE_PW 2
 
+#define OP_RESIZE 0xa
+// Opcode
+#define RESIZE_Opcode_LOW 0
+#define RESIZE_Opcode_HIGH 3
+#define RESIZE_Opcode_COUNT 4
+// Width of the input image
+#define RESIZE_IW_LOW 4
+#define RESIZE_IW_HIGH 13
+#define RESIZE_IW_COUNT 10
+// Height of the input image
+#define RESIZE_IH_LOW 14
+#define RESIZE_IH_HIGH 23
+#define RESIZE_IH_COUNT 10
+// Channel count for the input
+#define RESIZE_IC_LOW 24
+#define RESIZE_IC_HIGH 35
+#define RESIZE_IC_COUNT 12
+#define RESIZE_ImageStartAddress_LOW 36
+#define RESIZE_ImageStartAddress_HIGH 67
+#define RESIZE_ImageStartAddress_COUNT 32
+#define RESIZE_ImageEndAddress_LOW 68
+#define RESIZE_ImageEndAddress_HIGH 99
+#define RESIZE_ImageEndAddress_COUNT 32
+
 #define ZerothStartAddress_LOW 0
 #define ZerothStartAddress_HIGH 31
 #define ZerothStartAddress_COUNT 32
@@ -482,6 +506,7 @@ struct pretty_data {
   Table eltwise;
   Table transpose;
   Table reshape;
+  Table resize;
 
   void clear() {
     conv.clear();
@@ -493,6 +518,7 @@ struct pretty_data {
     eltwise.clear();
     transpose.clear();
     reshape.clear();
+    resize.clear();
   }
 };
 inline Table get_conv_table(const std::bitset<INST_SIZE_BITS>& inst) {
@@ -793,6 +819,26 @@ inline void pretty_print_reshape(const std::bitset<INST_SIZE_BITS>& inst) {
 	auto tbl = get_reshape_table(inst);
 	print_table(tbl);
 }
+inline Table get_resize_table(const std::bitset<INST_SIZE_BITS>& inst) {
+	Table tbl;
+	tbl.tbl.insert({"Opcode", bitset_range_get<RESIZE_Opcode_COUNT, INST_SIZE_BITS>(inst, RESIZE_Opcode_LOW, RESIZE_Opcode_HIGH)});
+	tbl.order.push_back("Opcode");
+	tbl.tbl.insert({"IW", bitset_range_get<RESIZE_IW_COUNT, INST_SIZE_BITS>(inst, RESIZE_IW_LOW, RESIZE_IW_HIGH)});
+	tbl.order.push_back("IW");
+	tbl.tbl.insert({"IH", bitset_range_get<RESIZE_IH_COUNT, INST_SIZE_BITS>(inst, RESIZE_IH_LOW, RESIZE_IH_HIGH)});
+	tbl.order.push_back("IH");
+	tbl.tbl.insert({"IC", bitset_range_get<RESIZE_IC_COUNT, INST_SIZE_BITS>(inst, RESIZE_IC_LOW, RESIZE_IC_HIGH)});
+	tbl.order.push_back("IC");
+	tbl.tbl.insert({"ImageStartAddress", bitset_range_get<RESIZE_ImageStartAddress_COUNT, INST_SIZE_BITS>(inst, RESIZE_ImageStartAddress_LOW, RESIZE_ImageStartAddress_HIGH)});
+	tbl.order.push_back("ImageStartAddress");
+	tbl.tbl.insert({"ImageEndAddress", bitset_range_get<RESIZE_ImageEndAddress_COUNT, INST_SIZE_BITS>(inst, RESIZE_ImageEndAddress_LOW, RESIZE_ImageEndAddress_HIGH)});
+	tbl.order.push_back("ImageEndAddress");
+	return tbl;
+}
+inline void pretty_print_resize(const std::bitset<INST_SIZE_BITS>& inst) {
+	auto tbl = get_resize_table(inst);
+	print_table(tbl);
+}
 
 inline void pretty_print(const std::bitset<INST_SIZE_BITS> &inst) {
   int op_code = extract_opcode(inst);
@@ -823,6 +869,9 @@ inline void pretty_print(const std::bitset<INST_SIZE_BITS> &inst) {
     break;
   case OP_RESHAPE:
     pretty_print_reshape(inst);
+    break;
+  case OP_RESIZE:
+    pretty_print_resize(inst);
     break;
   default:
     log_fatal("can't pretty print instruction with opcode {}\n", op_code);
@@ -865,6 +914,9 @@ inline void pretty_print_html(const std::bitset<INST_SIZE_BITS> &inst,
   case OP_RESHAPE:
     inst_data.reshape = get_reshape_table(inst);
     break;
+  case OP_RESIZE:
+    inst_data.resize = get_resize_table(inst);
+    break;
   default:
     log_fatal("can't pretty print instruction with opcode {}\n", op_code);
     break;
@@ -904,6 +956,7 @@ inline std::string generate_pretty(const pretty_data &pd, int index) {
   html << generate_table_html("v  Elt Wise", pd.eltwise);
   html << generate_table_html("v  TRANSPOSE", pd.transpose);
   html << generate_table_html("v  RESHAPE", pd.reshape);
+  html << generate_table_html("v  RESIZE", pd.resize);
   html << "</div>\n";
   return html.str();
 }
